cb0d978e173b3c08ce5a436b54f8a172a3f79ff1
Shubham Sharma <142394297+Shubhamsharma2002@users.noreply.github.com>
2026-05-27 15:56:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function shortcut(s1, s2) {
-   if (str1 === "" || str2 === "") {
+
+  if (s1 === "" || s2 === "") {
     return "";
   }
 
-  return str1[0] + str2[0];
+  return s1[0] + s2[0];
 }
 
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
+
 alert(shortcut(s1, s2));
